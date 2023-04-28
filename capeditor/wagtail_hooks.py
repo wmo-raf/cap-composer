895dad072c073fd8f73e40c70be188c963ca42b4
Grace Amondi <miswa.grace@gmail.com>
2023-04-28 14:08:52 +0300

    external cap editor page

--- capeditor/wagtail_hooks.py
+++ capeditor/wagtail_hooks.py
@@ -2,7 +2,12 @@
 from wagtail import hooks
 from django.utils.html import format_html
 from django.templatetags.static import static
-
+from capeditor.models import Alert
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin, 
+    modeladmin_register,
+    ModelAdminGroup
+)
 
 @hooks.register("insert_editor_js")
 def insert_editor_js():
@@ -17,3 +22,13 @@ def insert_global_admin_css():
         '<link rel="stylesheet" type="text/css" href="{}">',
         static("css/admin.css"),
     )
+
+class CAPAdmin(ModelAdmin):
+    model = Alert
+    menu_label = 'CAP Alerts'
+    menu_icon = 'cog'
+    menu_order = 200 
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+
+modeladmin_register(CAPAdmin)
