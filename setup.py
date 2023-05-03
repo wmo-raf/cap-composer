3c50790dbcab8a1721313b6a308622173bf006fb
Grace Amondi <miswa.grace@gmail.com>
2023-05-03 17:06:11 +0300

    add readme pypi

--- setup.py
+++ setup.py
@@ -1,20 +1,54 @@
-from setuptools import setup, find_packages
+import os
+import uuid
+
+from setuptools import find_packages
+from distutils.core import setup
+
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_requirements = parse_requirements(os.path.join(PROJECT_ROOT, 'requirements.txt'), session=uuid.uuid1())
+
+# e.g. ['django', 'google-api-python-client']
+requirements = [getattr(ir, 'requirement', str(getattr(ir, 'req', None))) for ir in install_requirements]
 
 setup(
-    name='capeditor',
-    version='0.1.3',
-    description='Wagtail CAP Editor can be used as standalone or integrated into website',
+    name='cap-editor',
+    version='0.1.4',
+    packages=find_packages(),
+    install_requires=requirements,
+    include_package_data=True,
+    license='MIT License',
+    description='Wagtail CAP Editor can be run as standalone or integrated into website',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/wmo-raf/cap-editor',
     author='Grace Amondi',
     author_email='miswa.grace@gmail.com',
-    install_requires=[
-        'Django>=4.0.0',
-        'djangorestframework-xml>=2.0.0',
-        'six>=1.16.0',
-        'wagtail>=4.0.0',
-        'wagtail-cache>=2.2.0',
-        'psycopg2-binary>=2.9.5',
-        'wagtail-lazyimages>=0.1.5'
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
     ],
-    include_package_data=True,
-    packages=find_packages(),
 )
